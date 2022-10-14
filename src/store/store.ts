4bd65ddcbc53ef3d3abbcc8cc29e8b90b7117823
NoDiggity.Dexter <dimonshinkarenko2001@gmail.com>
2022-10-14 19:06:21 +0300

    redux store in no longer displayed in production

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,7 +1,7 @@
-import {configureStore} from '@reduxjs/toolkit';
-import {rootReducer} from './root-reducer';
+import { configureStore } from '@reduxjs/toolkit';
+import { rootReducer } from './root-reducer';
 
 export const store = configureStore({
-    reducer: rootReducer,
-    devTools: true
+	reducer: rootReducer,
+	devTools: process.env.NODE_ENV !== 'production',
 });
